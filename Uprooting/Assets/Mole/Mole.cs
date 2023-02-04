446da6095edfb0c3da0e6734f1cb8301e959302c
Flavia Brogle <flavia.brogle@hotmail.com>
2023-02-04 15:42:48 +0100

    Add first basic version of grid based mole movement

--- Uprooting/Assets/Mole/Mole.cs
+++ Uprooting/Assets/Mole/Mole.cs
@@ -1,22 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using TurnBasedSystem;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[RequireComponent(typeof(Rigidbody2D))]
 public class Mole : MonoBehaviour
 {
     [SerializeField]
     [Tooltip("Max speed of mole in units per second.")]
     private float speed = 1f;
+    
+    [SerializeField]
+    [Tooltip("The tile coordinates of the mole's starting position.")]
+    private Vector2Int startPosition;
+    
+    private bool isMoving = false;
+    private MovementType movementType = MovementType.None;
+    private float movementPercent = 0f;
+    private Tile currTile;
+    private Tile nextTile;
 
-    private Rigidbody2D body;
-    private Vector2 move = Vector2.zero;
-
-    public void Start()
-    {
-        body = GetComponent<Rigidbody2D>();
+    public void Start() {
+        Tilemap.Instance.TryGetTile(startPosition.x, startPosition.y, out currTile);
+        transform.position = new Vector3(currTile.Location.x, currTile.Location.y, 0);
     }
 
     public void Update()
@@ -26,12 +33,32 @@ public class Mole : MonoBehaviour
 
     public void FixedUpdate()
     {
-        body.velocity = move * speed;
+        // body.velocity = move * speed;
+        if (isMoving)
+        {
+            movementPercent += Time.deltaTime * speed;
+            if (movementPercent >= 1f)
+            {
+                movementPercent = 0f;
+                currTile = nextTile;
+                isMoving = false;
+            }
+            transform.position = Vector2.Lerp(new Vector2(currTile.Location.x, currTile.Location.y), new Vector2(nextTile.Location.x, nextTile.Location.y), movementPercent);
+        }
+        
     }
 
     public void OnMove(InputValue value)
     {
-        move = value.Get<Vector2>();
-        body.velocity = move * speed;
+        if (isMoving) return;
+        var moveDirection = value.Get<Vector2>();
+        var moveDirectionInt = (Mathf.RoundToInt(moveDirection.x), Mathf.RoundToInt(moveDirection.y));
+        currTile.TryGetNeighbour(moveDirectionInt, out var possibleNextTile);
+        if (possibleNextTile == null) return; // TODO what to do when we are at the edge of the map?
+        
+        if (!Tilemap.Instance.TryGetMovementType(currTile, possibleNextTile, out var movementType))
+            return;
+        
+        isMoving = true;
     }
 }
