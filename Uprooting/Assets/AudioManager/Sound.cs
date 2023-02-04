dcad223511189d824096e8c60a8b647882490578
Leo.Steingart <32467430+Leocraft-LP@users.noreply.github.com>
2023-02-04 23:44:05 +0100

    Added hidden functionality
    Added functionality to set the Spacial Blend and the Stereo Pan.
    
    This functionality is hidden since I don't know if it is needed.

--- Uprooting/Assets/AudioManager/Sound.cs
+++ Uprooting/Assets/AudioManager/Sound.cs
@@ -14,7 +14,12 @@ public class Sound
     public float volume;
     [Range (0.1f, 3f)]
     public float pitch;
-
+    /* Optional, I dunno if it's needed:
+    [Range (0f, 1f)]
+    public float spacialBlend;
+    [Range (-1f, 1f)]
+    public float stereoPan;
+    */
     public bool loop;
 
     [HideInInspector]
