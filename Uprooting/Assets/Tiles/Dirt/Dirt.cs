9abbc42b4555e42ae0594231e02b97fbbee1147b
Flavia Brogle <flavia.brogle@hotmail.com>
2023-02-04 20:10:02 +0100

    Add digging logic

--- Uprooting/Assets/Tiles/Dirt/Dirt.cs
+++ Uprooting/Assets/Tiles/Dirt/Dirt.cs
@@ -8,10 +8,5 @@ public class Dirt : MonoBehaviour
     private Tile tile;
 
     public void Start() => tile = GetComponent<Tile>();
-
-    public void DigTunnel()
-    {
-        var success = tile.Tilemap.ReplaceTile(tile.Location, tile.Tilemap.TunnelTilePrefab);
-        Debug.Assert(success, "tunnel digging failed unexpectedly");
-    }
+    
 }
