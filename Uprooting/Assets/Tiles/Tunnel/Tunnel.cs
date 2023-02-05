d72be34b0cb34e2aa599f7f3021f65c6b8d1fd3c
TheJP <jp_55555@hotmail.com>
2023-02-05 01:40:15 +0100

    Implemented fg/bg tiles

--- Uprooting/Assets/Tiles/Tunnel/Tunnel.cs
+++ Uprooting/Assets/Tiles/Tunnel/Tunnel.cs
@@ -7,8 +7,25 @@ public class Tunnel : MonoBehaviour
 {
     private Tile tile;
 
-    public void Start() => tile = GetComponent<Tile>();
-    
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    public void Start()
+    {
+        tile = GetComponent<Tile>();
+        var background = Tilemap.Instance.BackgroundTunnelSpriteRenderer;
+        var x = spriteRenderer.bounds.min.x - background.bounds.min.x;
+        var y = spriteRenderer.bounds.min.y - background.bounds.min.y;
+        while (x < 0) x += background.bounds.size.x;
+        while (x > background.bounds.size.x) x -= background.bounds.size.x;
+        Debug.Assert(y >= 0);
+        var factor = background.sprite.pixelsPerUnit;
+        var crop = new Rect(x * factor, y * factor, 1f * factor, 1f * factor);
+        var sprite = Sprite.Create(background.sprite.texture, crop, new Vector2(0.5f, 0.5f), background.sprite.pixelsPerUnit);
+        spriteRenderer.sprite = sprite;
+        spriteRenderer.color = Color.white;
+    }
+
     /// <summary>
     /// Fills the tunnel with dirt.
     /// </summary>
