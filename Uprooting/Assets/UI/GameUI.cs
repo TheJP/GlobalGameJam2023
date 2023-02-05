844cf114083fa6e4fd1499091bc7fc3a0f0135a2
TheJP <jp_55555@hotmail.com>
2023-02-05 15:02:39 +0100

    Added enemy progress bar

--- Uprooting/Assets/UI/GameUI.cs
+++ Uprooting/Assets/UI/GameUI.cs
@@ -8,6 +8,7 @@ public class GameUI : MonoBehaviour
 {
     private Label actions;
     private VisualElement movement;
+    private ProgressBar enemyProgress;
 
     public void Start()
     {
@@ -16,10 +17,15 @@ public class GameUI : MonoBehaviour
         actions = ui.rootVisualElement.Q<Label>("actions");
         movement = ui.rootVisualElement.Q("movement");
         var nextRound = ui.rootVisualElement.Q<Button>("next-round");
+        var enemyBox = ui.rootVisualElement.Q("enemy-box");
+        enemyProgress = ui.rootVisualElement.Q<ProgressBar>("enemy-progress");
 
         FindObjectOfType<MoleOxygen>().HasOxygenChanged +=
             mole => noOxygen.style.display = mole.HasOxygen ? DisplayStyle.None : DisplayStyle.Flex;
         nextRound.RegisterCallback<ClickEvent>(e => TurnSystemController.Instance.EndPlayerTurn());
+
+        TurnSystemController.Instance.OnPlayerTurnEnd += (o, e) => enemyBox.style.display = DisplayStyle.Flex;
+        TurnSystemController.Instance.OnEnemyTurnEnd += (o, e) => enemyBox.style.display = DisplayStyle.None;
     }
 
     public void Update()
@@ -28,5 +34,7 @@ public class GameUI : MonoBehaviour
         var movementHeight = (float)TurnSystemController.Instance.CurrentMovementLeft /
             (float)TurnSystemController.Instance.PlayerMovementPerTurn;
         movement.style.height = new StyleLength(Length.Percent(movementHeight * 100f));
+
+        enemyProgress.value = EnemyController.Instance.Progress;
     }
 }
